23ea4058009865396c08abb3cacecd3e1312de1d
bakhridinova <bakhridinovaleyla@gmmail.com>
2023-01-22 01:15:32 +0500

    updated FunctionValueFinder.java, KeyValueSwapper.java, WordRepetitionMapCreator.java

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,20 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> stringIntegerMap = new HashMap<>();
+        for (Map.Entry<Integer, String> entry: sourceMap.entrySet()) {
+            if (stringIntegerMap.containsKey(entry.getValue())) {
+                if (entry.getKey() < stringIntegerMap.get(entry.getValue())) {
+                    stringIntegerMap.put(entry.getValue(), entry.getKey());
+                    continue;
+                }
+             }
+            stringIntegerMap.put(entry.getValue(), entry.getKey());
+        }
+        return stringIntegerMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,18 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        String[] words = sentence.toLowerCase().split("[!\"#$%&'()*+,\\-./:;<=>?@\\[\\]^_`{|}~ ’“”—‘]");
+        Map<String, Integer> wordCount = new LinkedHashMap<>();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+            }
+        }
+        return wordCount;
     }
 }
